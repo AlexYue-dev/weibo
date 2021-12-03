8c1e226f1871c292a849a1e1e4d6aa366b018691
AlexYue <yuexu0913@gmail.com>
2021-12-03 15:27:03 +0800

    用户退出登陆

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,22 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
-    //
+    /**
+     * 跳转到登陆界面
+     * @return Application|Factory|View
+     */
     public function create()
     {
         return view('sessions.create');
     }
 
     /**
+     * 用户登陆
      * @throws ValidationException
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
         $credentials = $this->validate($request, [
             'email' => 'required|email|max:255',
@@ -31,5 +40,16 @@ class SessionsController extends Controller
             session()->flash('danger', '很抱歉，邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
+    }
+
+    /**
+     * 退出登陆
+     * @return Application|RedirectResponse|Redirector
+     */
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success','您已成功推出');
+        return redirect('login');
     }
 }

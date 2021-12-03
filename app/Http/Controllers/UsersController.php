725f42e105088815e569ab6ff5215b9b04ca03fd
AlexYue <yuexu0913@gmail.com>
2021-12-03 16:05:48 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
@@ -22,7 +25,7 @@ class UsersController extends Controller
     /**
      * @throws ValidationException
      */
-    public function store(Request $request)
+    public function store(Request $request): \Illuminate\Http\RedirectResponse
     {
         $this->validate($request, [
             'name' => 'required|unique:users|max:50',
@@ -40,4 +43,36 @@ class UsersController extends Controller
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
+
+    /**
+     * 跳转到用户编辑界面
+     * @param User $user
+     * @return Application|Factory|View
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    /**
+     * 更新用户资料
+     * @throws ValidationException
+     */
+    public function update(User $user, Request $request): \Illuminate\Http\RedirectResponse
+    {
+        $this->validate($request,[
+            'name'=> 'required|max:50',
+            'password'=>'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password){
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+        return redirect()->route('users.show',$user->id);
+    }
+
+    
 }

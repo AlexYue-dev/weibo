3845f7d3cc228b73899f6ee55f27779ed56e20e6
AlexYue <yuexu0913@gmail.com>
2021-12-03 17:31:55 +0800

    管理员可删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,7 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
@@ -16,35 +17,49 @@ class UsersController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth',[
-            'except'=>['show','create','store'],
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store'],
         ]);
 
-        $this->middleware('guest',[
-            'only'=>['create'],
+        $this->middleware('guest', [
+            'only' => ['create'],
         ]);
     }
 
+    /**
+     * 用户列表
+     * @return Application|Factory|View
+     */
     public function index()
     {
         $users = User::paginate(6);
-        return \view('users.index',compact('users'));
+        return \view('users.index', compact('users'));
     }
 
-    public function create()
-    {
-        return view('users.create');
-    }
-
+    /**
+     * 个人中心
+     * @param User $user
+     * @return Application|Factory|View
+     */
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
 
     /**
+     * 跳转到用户注册界面
+     * @return Application|Factory|View
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * 用户注册
      * @throws ValidationException
      */
-    public function store(Request $request): \Illuminate\Http\RedirectResponse
+    public function store(Request $request): RedirectResponse
     {
         $this->validate($request, [
             'name' => 'required|unique:users|max:50',
@@ -58,6 +73,7 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
+        // 注册成功自动登陆
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
@@ -71,29 +87,43 @@ class UsersController extends Controller
      */
     public function edit(User $user)
     {
-        $this->authorize('update',$user);
-        return view('users.edit',compact('user'));
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
     }
 
     /**
      * 更新用户资料
-     * @throws ValidationException
+     * @throws ValidationException|AuthorizationException
      */
-    public function update(User $user, Request $request): \Illuminate\Http\RedirectResponse
+    public function update(User $user, Request $request): RedirectResponse
     {
-        $this->authorize('update',$user);
-        $this->validate($request,[
-            'name'=> 'required|max:50',
-            'password'=>'nullable|confirmed|min:6'
+        $this->authorize('update', $user);
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
         ]);
 
         $data = [];
         $data['name'] = $request->name;
-        if ($request->password){
+        if ($request->password) {
             $data['password'] = bcrypt($request->password);
         }
         $user->update($data);
-        return redirect()->route('users.show',$user->id);
+        return redirect()->route('users.show', $user->id);
+    }
+
+    /**
+     * 删除用户
+     * @param User $user
+     * @return RedirectResponse
+     * @throws AuthorizationException
+     */
+    public function destroy(User $user): RedirectResponse
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户');
+        return back();
     }
 
 
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -20,4 +20,15 @@ class UserPolicy
         return $currentUser->id === $user->id;
     }
 
+    /**
+     * 删除用户时的策略 1：是管理员 2：不能删除自己
+     * @param User $currentUser
+     * @param User $user
+     * @return bool
+     */
+    public function destroy(User $currentUser, User $user): bool
+    {
+        return $currentUser->is_admin && $currentUser->id !== $user->id;
+    }
+
 }

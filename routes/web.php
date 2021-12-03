06b857157ce6152c5a89572c998ce20c7d54baa5
AlexYue <yuexu0913@gmail.com>
2021-12-03 14:48:28 +0800

    创建回话

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-
+// 页面路由
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
-
+// 注册
 Route::get('signup', 'UsersController@create')->name('signup');
 
-
+// 用户路由
 Route::resource('users', 'UsersController');
+
+// 登陆推出路由
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');

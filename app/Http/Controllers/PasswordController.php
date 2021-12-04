767ab0986a69f5121b57f354ec029fac47f974ea
AlexYue <yuexu0913@gmail.com>
2021-12-04 18:04:27 +0800

    重置密码

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -7,6 +7,7 @@ use Carbon\Carbon;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Mail;
@@ -24,26 +25,31 @@ class PasswordController extends Controller
         return view('auth.passwords.email');
     }
 
-    public function sendResetLinkEmail(Request $request)
+    /**
+     * 发送认证邮件
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function sendResetLinkEmail(Request $request): RedirectResponse
     {
         // 1.验证邮箱
-        $request->validate(['email'=>'required|email']);
+        $request->validate(['email' => 'required|email']);
         $email = $request->email;
 
         // 2.获取对应的用户
-        $user = User::where('email',$email)->first();
+        $user = User::where('email', $email)->first();
 
         // 3.如果用户不存在
-        if (is_null($user)){
-            session()->flash('danger','邮箱未注册');
+        if (is_null($user)) {
+            session()->flash('danger', '邮箱未注册');
             return redirect()->back()->withInput();
         }
 
         // 4.生成重置密码的token
-        $token = hash_hmac('sha256',Str::random(40), config('app.key'));
+        $token = hash_hmac('sha256', Str::random(40), config('app.key'));
 
         // 5.将email & token 写入数据库 保证email的唯一性
-        DB::table('password_resets')->updateOrInsert(['email'=>$email],[
+        DB::table('password_resets')->updateOrInsert(['email' => $email], [
             'email' => $email,
             'token' => Hash::make($token),
             'created_at' => new Carbon,
@@ -54,7 +60,68 @@ class PasswordController extends Controller
             $message->to($email)->subject("忘记密码");
         });
 
-        session()->flash('success','重置密码邮件发送成功');
+        session()->flash('success', '重置密码邮件发送成功');
+        return redirect()->back();
+    }
+
+    /**
+     * 点击邮件链接，跳转到重置密码界面
+     * @param Request $request
+     * @return Application|Factory|View
+     */
+    public function showResetForm(Request $request)
+    {
+        $token = $request->route()->parameter('token');
+        return view('auth.passwords.reset', compact('token'));
+    }
+
+    public function reset(Request $request)
+    {
+        // 1.数据验证
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $email = $request->email;
+        $token = $request->token;
+        // 找回密码链接的有效时间
+        $expires = 60 * 10;
+
+        // 2. 获取对应的用户
+        $user = User::where('email', $email)->first();
+
+        // 3.如果找不到用户
+
+        if (is_null($user)) {
+            session()->flash('danger', '邮箱未注册');
+            return redirect()->back()->withInput();
+        }
+
+        // 4.读取重置记录
+        $record = (array)DB::table('password_resets')->where('email', $email)->first();
+
+        // 5.记录存在
+        if ($record) {
+            // 5.1 检查是否过期
+            if (Carbon::parse($record['created_at'])->addSecond($expires)->isPast()) {
+                session()->flash('danger', '链接已过期，请重新尝试');
+                return redirect()->back();
+            }
+            // 5.2 检查token是否正确
+            if (!hash::check($token, $record['token'])) {
+                session()->flash('danger', 'token错误');
+            }
+            // 5.3 一切正常，更新用户密码
+            $user->update(['passsword' => bcrypt($request->password)]);
+            // 5.4 提示用户更新密码成功
+            session()->flash('success', '密码更新成功');
+            return redirect()->route('login');
+        }
+
+        // 记录不存在
+        session()->flash('danger', '未找到重置记录');
         return redirect()->back();
     }
 }

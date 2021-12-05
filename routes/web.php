3745edb60dac61122b1b78dc2e28edb44d1c3117
AlexYue <yuexu0913@gmail.com>
2021-12-05 16:31:31 +0800

    发布博客

--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,7 @@ Route::get('password/reset',  'PasswordController@showLinkRequestForm')->name('p
 Route::post('password/email',  'PasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset',  'PasswordController@reset')->name('password.update');
+
+// 博客路由
+Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
